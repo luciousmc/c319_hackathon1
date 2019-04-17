dd5c181f50737c8aaf6051275e390edf596aa00b
Claudius King <tezcatlipoca7123@gmail.com>
2019-04-17 09:41:46 -0700

    Added firebase config

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCKjEn3WD8rbhXQmvemVkgvK2KjfOkihds",
+    authDomain: "cards-against-humanity-5b64b.firebaseapp.com",
+    databaseURL: "https://cards-against-humanity-5b64b.firebaseio.com",
+    projectId: "cards-against-humanity-5b64b",
+    storageBucket: "cards-against-humanity-5b64b.appspot.com",
+    messagingSenderId: "1019047172339"
+  };
+var defaultProject = firebase.initializeApp(firebaseConfig);
+window.onload = function(){
+  console.log(defaultProject.name);
+}
