472e67b8535f9cbf75d48bbd5d3b3fe72c6a2516
Claudius King <tezcatlipoca7123@gmail.com>
2019-04-17 16:41:57 -0700

    Removed the useless console logs

--- game.js
+++ game.js
@@ -1,16 +1,13 @@
 class Game {
   constructor() {
     this.gameDeck = this.getBlackCards(cardText);
-    this.playerDeck = this.getWhiteCards(cardText);;
+    this.playerDeck = this.getWhiteCards(cardText);
 
     this.players = [
       new Player("player1", true),
       new Player("player2", false),
       new Player("player3", false)
     ];
-
-    console.log(blackArr);
-    console.log(whiteArr);
   }
 
   //takes the json from cardtext.js as a parameter and returns an array of black cards
