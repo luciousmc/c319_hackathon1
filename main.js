58e75087896e972c44ebad93577d4933accf3227
Claudius King <tezcatlipoca7123@gmail.com>
2019-04-17 13:14:06 -0700

    Corrected the changes to Dan's code Added the main js file Added the game object

---
+++ main.js
@@ -0,0 +1,16 @@
+var backend = new GenericFBModel("Cards Against Humanity", onChange, firebaseOnload);
+backend.initialize();
+backend.start();
+
+function onChange(payload){
+  console.log("a change happened: ", payload);
+}
+
+function firebaseOnload(){
+  var game = new Game();
+  backend.saveState(game);
+  backend.getAllData(function(data){
+    console.log(data);
+  });
+}
+//getAllData grabs the data at any point needed at the beginning
