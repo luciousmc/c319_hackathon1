c0a43cf79392f9e5127f9368ae771607849e51e5
Claudius King <tezcatlipoca7123@gmail.com>
2019-04-18 12:51:29 -0700

    Added functionality for click handlers and made the displayed white card show which smaller white card was chosen Add the cards to the selected cards property of the game object Created the verifyPlayer function to check who the player is on a browser

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function onChange(payload){
 
 function firebaseOnload(){
   backend.getAllData(function(data){
+    $(".confirm-button").toggle();
     console.log("data is:",data);
     var playerName = null;
     var player = null;
@@ -34,12 +35,16 @@ function firebaseOnload(){
       }
       // game.players.push(new Player(playerNames.pop(), false));
     }
+
+
     console.log('Game:', game);
     console.log('player verification is:', playerVerification);
     for(var player of game.players){
       player.cards = deck.dealPlayerCards(5);
     }
     player.makePlayerArea();
+    $(".cardtext").on("click",handleCardClick);
+    $(".confirm-button").on("click",handleConfirmButtonClick);
     game.gameCard = deck.dealGameCard();
     console.log(game.gameCard);
     $(".card-black").text(game.gameCard.text);
@@ -50,6 +55,48 @@ function firebaseOnload(){
 
 }
 
-window.onload = function(){
+function handleCardClick(event){
+  if(verifyPlayer().turn){
+
+  }
+  else{
+    var whiteCardText = $(this).text();
+    $(".card-white").text(whiteCardText);
+    $(".confirm-button").toggle();
+  }
+}
+
+function handleConfirmButtonClick(){
+  var whiteCardText = $(".card-white").text();
+  for(var player of game.players){
+    for(var card of player.cards){
+      if(card.text === whiteCardText){
+        console.log(game);
+        card.owner = playerVerification;
+        if(game.selectedCards === undefined){
+          game.selectedCards = [card];
+        }
+        else{
+          game.selectedCards.push(card);
+        }
+        //deck.dealPlayerCards(1);
+        console.log(game);
+      }
+    }
+  }
+  var arrWhiteCards = $(".cardtext");
+  arrWhiteCards.each(function(index, element){
+    if($(element).text() === whiteCardText){
+      $(element).remove();
+    }
+  })
+}
+
+function verifyPlayer(){
+  for(var player of game.players){
+    if(player.name === playerVerification){
+      return player;
+    }
+  }
 }
 //getAllData grabs the data at any point needed at the beginning
