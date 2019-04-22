b6e877f24bac29796e57840913764aa9d4ecaf90
monsoonjunkie <anthonyle925@gmail.com>
2019-04-21 17:23:29 -0700

    remove some comments

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ class Game {
       gameDeck : gameDeck,
       gameCard : gameDeck.pop(),
       players : [],
-      playerNames : ['player3','player2','player1'],//,'player4','player3','player2','player1'];//,'player14','player13','player12','player11','player10','player9','player8','player7','player6','player5','player4','player3','player2','player1']
+      playerNames : ['Player 3','Player 2','Player 1'],//,'player4','player3','player2','player1'];//,'player14','player13','player12','player11','player10','player9','player8','player7','player6','player5','player4','player3','player2','player1']
       selecting : true,
       winningCardSelected : false,
       ready : 0
